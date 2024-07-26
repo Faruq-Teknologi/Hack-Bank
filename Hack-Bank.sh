5bad6651fe15c6867bf227a81f833fba8a9cd366
MR~TEKNO_~VRS <aakubiasaa334@gmail.com>
2024-07-26 16:36:46 +0700

    Add files via upload

---
+++ Hack-Bank.sh
@@ -0,0 +1,41 @@
+#!/system/bin/sh
+# by MR~TEKNO_~VRS
+# start
+clear
+# tampilan
+sleep 2
+toilet -f big -F gay Hack-Bank | lolcat
+sleep 1
+echo
+echo " ===================================== " | lolcat
+echo " {•} Author : MR~TEKNO_~VRS            " | lolcat
+echo " {•} Team   : Mavia Teknologi          " | lolcat
+echo " ======================================" | lolcat
+echo
+echo " masukkan nama bank (BRI/BCA/BNI) : " | lolcat
+   read -p " $ " bang 
+ echo
+ sleep 3
+echo
+  echo " masukkan nomor rekening" | lolcat
+ read -p " $ " pil
+  echo
+ sleep 1
+   echo " masukkan nominal" | lolcat
+   read -p " $ " nominall 
+  echo
+  echo " $bang nomor $pil telah di transfer sebesar $nominall" | lolcat
+    echo
+   sleep 2
+   echo " # gunakan dengan bijak " | lolcat
+  echo " done " | lolcat
+   sleep 2
+   echo " keluar" | lolcat
+   sleep 4
+   echo " keluar 3"
+   sleep 2
+   echo " keluar 2"
+   sleep 2
+   echo " keluar 1"
+  sleep 7
+  clear
